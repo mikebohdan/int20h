e517d40667c43e2f3f56a52079d7c3f7e20c9841
mikebohdan <darkcore27@gmail.com>
2016-02-28 10:33:46 +0200

    just sync

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, \
  url_for, send_from_directory
 from werkzeug.wrappers import Response
 from werkzeug import secure_filename
+from flask.ext.sqlalchemy import SQLAlchemy
 import tools.classifier
 import pickle
 
@@ -13,15 +14,25 @@ ALLOWED_EXTENSIONS = set(['txt', 'csv', 'tsv'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.dirname(os.path.realpath(__file__)) + "/uploads"
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
+
+class Example(db.Model):
+    __tablename__ = "examples"
+    a = db.Column(db.LargeBinary(), primary_key=True)
+
+try:
+    db.create_all()
+except Exception:
+    pass
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
@@ -29,18 +40,15 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             mw = tools.classifier.mentoryWEB(os.path.dirname(os.path.realpath(__file__)) + "/uploads/" + file.filename)
-            temp = pickle.dumps(mw)
-            
-    return render_template('upload.html')
+            db.session.add(Exception(a=pickle.dumps(mw)))
+            return
+    else:
+        print(get_url)
+        return render_template('upload.html')
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        return render_template('index.html')
-    elif request.method == 'POST':
-        data = json.loads(request.data.decode())
-        odata = json.dumps(data)
-        return Response(odata, mimetype='text/json')
+@app.route('/api/<id>', methods=['POST'])
+def api(id):
+    pass
 
 if __name__ == '__main__':
     app.debug = True
